1486a73ba631b8fd9af751512b243a0a100f7356
gregnorth17 <greg_north@hotmail.co.uk>
2023-09-22 20:15:02 -0600

    second commit

--- src/components/Lineups.jsx
+++ src/components/Lineups.jsx
@@ -1,11 +1,40 @@
+import { Box, Typography } from '@mui/material'
 
 const Lineups = ({lineups}) => {
-	console.log(lineups)
+	// console.log(lineups)
 
-	
-	return (
-		<div>Lineups</div>
-	)
+	// lineups &&
+	// lineups.map((lineup, index) => {
+	// 	return (
+	// 		<Box key={index}>
+	// 			{lineup.team.name}
+	// 		</Box>
+	// 	)
+	// })
+	return lineups.map((lineup, index) => {
+		console.log(lineup)
+		return (
+			<Box key={index}>
+				<Box sx={{
+					display: 'grid',
+					gridTemplateColumns: 'repeat(4, 1fr)',
+				}}>
+					<Typography>{lineup.team.name}</Typography>
+					<Typography>{lineup.formation}</Typography>
+					<Box sx={{
+						width: '28px',
+						height: '28px',
+						gridColumn: 4
+					}}>
+						<img src={lineup.team.logo} alt='team badge' />
+					</Box>
+				</Box>
+				<Box>
+					<Typography component='h2'>{lineup.}</Typography>
+				</Box>
+			</Box>
+		)
+	})
 }
 
 export default Lineups
