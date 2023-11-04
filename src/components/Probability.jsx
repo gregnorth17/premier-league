6c07d8fe8557fbc7ed6c886d5e3c05de577b6557
gregnorth17 <greg_north@hotmail.co.uk>
2023-11-04 17:11:18 -0600

    probability

--- src/components/Probability.jsx
+++ src/components/Probability.jsx
@@ -4,31 +4,25 @@ const Probability = ({percent, teams}) => {
 	console.log(percent)
 	console.log(teams)
 	return (
-		<table style={{margin:'0 auto'}} width='90%'>
-			<thead>
-				{/* <tr>
-					<th colSpan='3'>WIN PROBABILITY</th>
-				</tr> */}
-				<tr>
-					<th style={{width: '33%'}} align="left">{teams.home.name}</th>
-					<th style={{width: '33%'}} align="center">Draw</th>
-					<th style={{width: '33%'}} align="right">{teams.away.name}</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>{percent.home}</td>
-					<td>{percent.draw}</td>
-					<td>{percent.away}</td>
-				</tr>
-				<tr>
-					<td style={{background: '#FFB4A4', height: '10px', width: `${percent.home}`}}></td>
-					<td style={{background: '#D6D6D6', width: `${percent.draw}`}}></td>
-					<td style={{background: '#FFB4A4', width: `${percent.away}`}}></td>
-				</tr>
-			</tbody>
-			
-		</table>
+		<div style={{
+			margin:'0 auto',
+			display: 'grid',
+			gridTemplateColumns: 'repeat(3, 1fr)',
+			width: '90%'
+		}}>
+			<div style={{gridColumn: '1 / span 3', color: '#bdc1c6', textAlign:'center', fontWeight: '800', marginTop: '1em', fontSize: '.75rem'}}>WIN PROBABILITY</div>
+			<div style={{textAlign: 'left', color: '#bdc1c6', fontWeight: '600', fontSize: '.75rem'}} width='33%'>{teams.home.name}</div>
+			<div style={{textAlign: 'center', color: '#bdc1c6', fontWeight: '600', fontSize: '.75rem'}}  width='33%'>Draw</div>
+			<div style={{textAlign: 'right', color: '#bdc1c6', fontWeight: '600', fontSize: '.75rem'}}  width='33%'>{teams.away.name}</div>
+			<div style={{textAlign: 'left', color: '#FFB4A4', fontSize: '.75rem'}} width='33%'>{percent.home}</div>
+			<div style={{textAlign: 'center', color: '#D6D6D6', fontSize: '.75rem'}}  width='33%'>{percent.draw}</div>
+			<div style={{textAlign: 'right', color: '#FFB4A4', paddingBottom: '1em', fontSize: '.75rem'}}  width='33%'>{percent.away}</div>
+			<div style={{display: 'flex', gridColumn: '1 / span 3'}}>
+				<div style={{background: '#FFB4A4', height: '10px', width: `${percent.home}`, borderRight: '1px solid #212121'}}></div>
+				<div style={{background: '#D6D6D6', width: `${percent.draw}`}}></div>
+				<div style={{background: '#FFB4A4', width: `${percent.away}`, borderLeft: '1px solid #212121'}}></div>
+			</div>
+		</div>
 	)
 }
 
