6dbb3cc3f500d42306454137eb00269b32e4b402
gregnorth17 <greg_north@hotmail.co.uk>
2023-11-06 15:24:57 -0600

    team pages add

--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useSearchParams } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
 import { fetchFixtures } from '../api';
 import Fixture from '../components/Fixture';
 import Fixtures from '../components/Fixtures';
@@ -7,24 +7,34 @@ const fixturesLoader = () => (fetchFixtures())
 
 
 const Matches = () => {
-	// console.log(fixtures)
 
+	const {id : paramsId} = useParams()
 	const fixtures = useLoaderData()
 
-	const [searchParams, setSearchParams] = useSearchParams()
+	console.log(paramsId)
+	console.log(fixtures)
+	// const [searchParams, setSearchParams] = useSearchParams()
 
-	const typeFilter = searchParams.get('type')
+	// const typeFilter = searchParams.get('type')
 
-	const displayFixtures = typeFilter ? 
-		fixtures.filter(({teams: {away: {name: awayTeam}, home: {name: homeTeam}}}) =>
-			awayTeam.toLowerCase() === typeFilter || 
-			homeTeam.toLowerCase() === typeFilter) 
-			:
+	// const displayFixtures = typeFilter ? 
+	// 	fixtures.filter(({teams: {away: {name: awayTeam}, home: {name: homeTeam}}}) =>
+	// 		awayTeam.toLowerCase() === typeFilter || 
+	// 		homeTeam.toLowerCase() === typeFilter) 
+	// 		:
+	// 	fixtures
+
+	// console.log(displayFixtures)
+
+	// console.log(typeFilter)
+
+	const displayFixtures = paramsId ?
+		fixtures.filter(({teams: {away: {id: awayId},home: {id: homeId}}}) => awayId == paramsId || homeId == paramsId) :
 		fixtures
 
 	console.log(displayFixtures)
 
-	console.log(typeFilter)
+
 
 	return (
 		<Fixtures>
