90c5cf0c6dc2821baf37c5303d3946a8ea1ae1e6
gregnorth17 <greg_north@hotmail.co.uk>
2023-11-14 14:50:36 -0600

    team navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { createContext, useState } from 'react'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './App.css'
 import Error from './components/Error'
@@ -6,19 +7,21 @@ import { leagueTableLoader } from './components/LeagueTableLayout'
 import Lineups from './components/Lineups'
 import MatchDetailsLayout, { fixtureLoader } from './components/MatchDetailsLayout'
 import Stats, { probabilityLoader } from './components/Stats'
-import TeamPage from './components/TeamPage'
+import TeamPage, { teamPageLoader } from './components/TeamPage'
 import Home from './pages/Home'
 import Matches, { fixturesLoader } from './pages/Matches'
 import NotFound from './pages/NotFound'
 import SeasonStats, { seasonStatsLoader } from './pages/SeasonStats'
 
-
-// const YearContext = createContext()
-
-// export { YearContext }
+const Context = createContext()
 
 const App = () => {
-
+	// const YearContext = createContext()
+	// export { YearContext }
+	
+	
+	const [team, setTeam] = useState({})
+	console.log(team)
 	const router = createBrowserRouter(createRoutesFromElements(
 		<>
 			<Route path='/' element={<Layout />}  >
@@ -28,7 +31,8 @@ const App = () => {
 				<Route path='seasonstats' loader={seasonStatsLoader} element={<SeasonStats />} />
 				<Route path='*' element={<NotFound />} />
 			</Route>
-			<Route path=':id' loader={fixturesLoader} element={<TeamPage />} />
+			<Route path=':id/matches' loader={fixturesLoader} action={teamPageLoader} element={<TeamPage />} />
+			<Route path=':id/matches' loader={fixturesLoader} action={teamPageLoader} element={<TeamPage />} />
 			<Route path='matches/:fixtureId/' element={<MatchDetailsLayout />} loader={fixtureLoader}>
 				<Route index element={<Stats />} loader={probabilityLoader}  />
 				<Route path='lineups' element={<Lineups />}  />
@@ -37,8 +41,11 @@ const App = () => {
 	))
 
   return (
-		<RouterProvider router={router} />
+		<Context.Provider value={{team, setTeam}}>
+			<RouterProvider router={router} />
+		</Context.Provider>
   )
 }
-
+	
+export { Context }
 export default App
