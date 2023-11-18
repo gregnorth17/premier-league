a638900d91c7a77d27633faf971762461285cf32
gregnorth17 <greg_north@hotmail.co.uk>
2023-11-17 21:17:32 -0600

    filter results buttons

--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -1,7 +1,9 @@
-import { Button } from '@mui/material'
 import { useSearchParams } from 'react-router-dom'
 import Matches from '../pages/Matches'
 // import NavBar from './NavBar'
+import line from '../assets/draw.svg'
+import cross from '../assets/lose.svg'
+import tick from '../assets/win.svg'
 
 
 const teamPageLoader = ({ params }) => {
@@ -18,10 +20,33 @@ const TeamPage = () => {
 	return (
 		<>
 		<div>
-			<Button variant='outlined' color='error' onClick={() => setSearchParams({result: 'win'})}>Win</Button>
-			<Button variant='outlined' color='error' onClick={() => setSearchParams({result: 'lose'})}>Lose</Button>
-			<Button variant='outlined' color='error' onClick={() => setSearchParams({result: 'draw'})}>Draw</Button>
-			<Button variant='outlined' color='error' onClick={() => setSearchParams({})}>Clear</Button>
+				<h3 style={{color: '#bdc1c6'}}>Filter results</h3>
+			<div style={{display: 'flex'}}>
+
+				<button onClick={() => setSearchParams({result: 'win'})} 
+								className='filter-button'>
+					<img src={tick} alt="Green tick" />
+					Win
+				</button>
+
+				<button onClick={() => setSearchParams({result: 'lose'})} 
+								className='filter-button'>
+					<img src={cross} alt="Red cross" />
+					Lose
+				</button>
+
+				<button onClick={() => setSearchParams({result: 'draw'})} 
+								className='filter-button'>
+					<img style={{marginRight: '.5em'}} src={line} alt="Grey" />
+					Draw
+				</button>
+
+				<button onClick={() => setSearchParams({})} 
+								className='filter-button'>
+					Clear
+				</button>
+
+			</div>
 			<Matches resultFilter={resultFilter} />
 			{/* <Form>
 				<input type="text" placeholder='Name' />
