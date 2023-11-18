be43c47ca7acf6387c07f4b5d2d7284247054330
gregnorth17 <greg_north@hotmail.co.uk>
2023-11-17 21:49:30 -0600

    filter buttons

--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -20,24 +20,30 @@ const TeamPage = () => {
 	return (
 		<>
 		<div>
-				<h3 style={{color: '#bdc1c6'}}>Filter results</h3>
-			<div style={{display: 'flex'}}>
+			<div style={{display: 'flex', alignItems: 'center', width: '650px', margin: '0 auto 8px'}}>
+				<h2 style={{fontSize: '.875rem', color: '#bdc1c6', margin: '0'}}>Filter results:</h2>
 
 				<button onClick={() => setSearchParams({result: 'win'})} 
-								className='filter-button'>
+								className={`
+									filter-button
+									${resultFilter === 'win' ? 'selected' : ''}`}>
 					<img src={tick} alt="Green tick" />
 					Win
 				</button>
 
 				<button onClick={() => setSearchParams({result: 'lose'})} 
-								className='filter-button'>
+								className={`
+									filter-button
+									${resultFilter === 'lose' ? 'selected' : ''}`}>
 					<img src={cross} alt="Red cross" />
 					Lose
 				</button>
 
 				<button onClick={() => setSearchParams({result: 'draw'})} 
-								className='filter-button'>
-					<img style={{marginRight: '.5em'}} src={line} alt="Grey" />
+								className={`
+									filter-button
+									${resultFilter === 'draw' ? 'selected' : ''}`}>
+					<img style={{marginRight: '.5em'}} src={line} alt="Grey line" />
 					Draw
 				</button>
 
